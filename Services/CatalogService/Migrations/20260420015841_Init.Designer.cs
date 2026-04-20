878b446c53fe9e5f3990dc24b8f17d8320789449
khangdepzaivodich <phantuankhanghcmue@gmail.com>
2026-04-20 08:59:22 +0700

    fix some bugs

--- Services/CatalogService/Migrations/20260420015841_Init.Designer.cs
+++ Services/CatalogService/Migrations/20260420015841_Init.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CatalogService.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260420013742_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20260420015841_Init")]
+    partial class Init
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
