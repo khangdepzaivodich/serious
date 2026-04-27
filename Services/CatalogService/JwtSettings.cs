8f863fc557cd92eb942f24c796c73ebb5de168f8
khangdepzaivodich <phantuankhanghcmue@gmail.com>
2026-04-27 18:41:05 +0700

    feat: add jwt Auth in CatalogService

---
+++ Services/CatalogService/JwtSettings.cs
@@ -0,0 +1,9 @@
+namespace CatalogService
+{
+    public class JwtSettings
+    {
+        public string? RsaPublicKey { get; set; }
+        public required string Issuer { get; set; }
+        public required string Audience { get; set; }
+    }
+}
